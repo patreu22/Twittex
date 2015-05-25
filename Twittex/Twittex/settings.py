c5cb2c31f8346aad39e2ef1d25cd859dc48f4623
patreu22 <patrickreuter@outlook.com>
2015-05-25 16:10:06 +0200

    Settings edited

--- Twittex/Twittex/settings.py
+++ Twittex/Twittex/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'Twittex.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'twittextest',
-        'USER': 'patrick',
+        'NAME': 'twittextestdb',
+        'USER': 'django',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
