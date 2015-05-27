8bf29665812e4c2ee7694f39a1d25978501c8752
Duy Bach Nguyen <duybach@live.de>
2015-05-27 11:56:24 +0200

    urls.py updated

--- Twittex/twittexApp/urls.py
+++ Twittex/twittexApp/urls.py
@@ -15,3 +15,15 @@ urlpatterns = patterns('',
     ),
     url('^', include('django.contrib.auth.urls')),
 )
+
+"""
+used urls in url('^', include('django.contrib.auth.urls'))
+^login/$ [name='login']
+^logout/$ [name='logout']
+^password_change/$ [name='password_change']
+^password_change/done/$ [name='password_change_done']
+^password_reset/$ [name='password_reset']
+^password_reset/done/$ [name='password_reset_done']
+^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$ [name='password_reset_confirm']
+^reset/done/$ [name='password_reset_complete']
+"""
