2565a2f8a30609006c0a8b50a9ee4be31bdbf661
Ron W <ron.w939@gmail.com>
2015-06-05 00:00:29 +0200

    Nachricht schreiben fertig, anzeigen begonnen

--- Twittex/twittexApp/views.py
+++ Twittex/twittexApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from itertools import chain
 from django.views.generic import TemplateView, CreateView, ListView
 from twittexApp.forms import UserCreationForm, AuthenticationForm
 from twittexApp.models import User, Posts, Nachrichten
@@ -20,14 +21,29 @@ class HomeView(ListView):
     template_name = 'home.html'
     model = Posts
     success_url ='/home/'
-
-class NachrichtenView(ListView):
-    template_name = 'nachrichten.html'
-    model = Nachrichten
-    success_url ='/home/'
 	
 #called by submit post
 def newPost(request):
     p = Posts(absender = str(request.user), inhalt = str(request.POST.get('inhalt')), hashtags='NULL', mentioned='NULL')
     p.save()
     return redirect('/home/')
+
+#Nachrichten
+class NewMsgView(ListView):
+    template_name = 'newMsg.html'
+    def get_queryset(self):
+        queryset = Nachrichten.objects.all()
+
+def sendMsg(request):
+    e = str(request.POST.get('recipient'))
+    n = Nachrichten(absender = str(request.user), inhalt = str(request.POST.get('inhalt')), empfaenger = e)
+    n.save()
+    return redirect('/Messages/'+e)
+    
+class NachrichtenView(ListView):
+    template_name = 'nachrichten.html'
+    def get_queryset(self):
+        q1 = Nachrichten.objects.filter(empfaenger = str(self.request.user))
+        q2 = Nachrichten.objects.filter(absender = str(self.request.user))
+        return list(chain(q2, q1))
+    
