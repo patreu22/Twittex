25cb44fabcead8650edaadea5ca84516f5cc5d00
Duy Bach Nguyen <duybach@live.de>
2015-06-08 12:13:08 +0200

    profile ready

--- Twittex/twittexApp/forms.py
+++ Twittex/twittexApp/forms.py
@@ -10,10 +10,20 @@ class newPostForm(ModelForm):
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'password2')
+
+    def clean(self):
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+
+        if password and password != password2:
+            raise forms.ValidationError("Passwords don't match")
+
+        return self.cleaned_data
 
 
 class UserProfileForm(forms.ModelForm):
--- Twittex/twittexApp/views.py
+++ Twittex/twittexApp/views.py
@@ -9,64 +9,43 @@ from django.http import HttpResponse
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-"""
-class RegisterView(CreateView):
-    template_name = 'register.html'
-    model = UserProfile
-    success_url = '/login/'
-    form_class = UserCreationForm
-"""
 
 def register(request):
     # Like before, get the request's context.
     context = RequestContext(request)
 
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
+    # will change when registration done
     registered = False
 
-    # If it's a HTTP POST, we're interested in processing form data.
+    # User input
     if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
 
-        # If the two forms are valid...
+        # if valid save
         if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data to the database.
             user = user_form.save()
 
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
             user.set_password(user.password)
             user.save()
 
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves, we set commit=False.
-            # This delays saving the model until we're ready to avoid integrity problems.
+            # create but dont save yet
             profile = profile_form.save(commit=False)
             profile.user = user
 
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and put it in the UserProfile model.
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
 
-            # Now we save the UserProfile model instance.
+            # now save
             profile.save()
 
-            # Update our variable to tell the template registration was successful.
+            # say true for template
             registered = True
 
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
         else:
             print (user_form.errors, profile_form.errors)
 
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
+    # provie forms
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
