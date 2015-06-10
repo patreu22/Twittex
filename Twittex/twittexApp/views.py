343c501d0401f96125fb476bb5263fef37917b70
Duy Bach Nguyen <duybach@live.de>
2015-06-10 16:37:48 +0200

    profile/<username> now working

--- Twittex/twittexApp/views.py
+++ Twittex/twittexApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, RequestContext, render_to_response
+from django.shortcuts import render, redirect, RequestContext, render_to_response, Http404
 from itertools import chain
 from django.db.models import Q
 from django.views.generic import TemplateView, CreateView, ListView, DetailView
@@ -45,7 +45,7 @@ def register(request):
             registered = True
 
         else:
-            print (user_form.errors, profile_form.errors)
+            print(user_form.errors, profile_form.errors)
 
     # provie forms
     else:
@@ -54,9 +54,9 @@ def register(request):
 
     # Render the template depending on the context.
     return render_to_response(
-            'register.html',
-            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
-            context)
+        'register.html',
+        {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
+        context)
 
 
 class HomeView(ListView):
@@ -64,46 +64,73 @@ class HomeView(ListView):
     model = Posts
     success_url = '/home/'
 
+
+"""
 class ProfileDetailView(ListView):
     template_name = 'profile.html'
     model = Posts
     success_url = '/profile/'
+"""
 
-#called by submit post
+"""
+class ProfileDetailView(DetailView):
+    template_name = 'profile.html'
+    slug_field = 'username'
+    model = User
+
+    def get_queryset(self):
+        posts = Posts.objects.all
+        return posts
+"""
+
+
+def ProfileDetailView(request, username):
+    posts = Posts.objects.all()
+    user = User.objects.get(username=username)
+    return render_to_response('profile.html', {'object_list': posts, 'user': user, 'request': request})
+
+
+# called by submit post
 def newPost(request):
-    p = Posts(absender = str(request.user), inhalt = str(request.POST.get('inhalt')), hashtags='NULL', mentioned='NULL')
+    p = Posts(absender=str(request.user), inhalt=str(request.POST.get('inhalt')), hashtags='NULL', mentioned='NULL')
     p.save()
     return redirect('/home/')
+
 
 #Nachrichten
 class NewMsgView(ListView):
     template_name = 'newMsg.html'
     context_object_name = 'conversations'
+
     def get_queryset(self):
         usr = str(self.request.user)
-        q = Nachrichten.objects.filter(Q(absender = usr)
-                                              | Q(empfaenger = usr)).exclude(empfaenger = usr).values('empfaenger').distinct('empfaenger')
+        q = Nachrichten.objects.filter(Q(absender=usr)
+                                       | Q(empfaenger=usr)).exclude(empfaenger=usr).values('empfaenger').distinct(
+            'empfaenger')
         return q
+
 
 def sendMsg(request):
     e = str(request.POST.get('recipient'))
-    n = Nachrichten(absender = str(request.user), inhalt = str(request.POST.get('inhalt')), empfaenger = e)
+    n = Nachrichten(absender=str(request.user), inhalt=str(request.POST.get('inhalt')), empfaenger=e)
     n.save()
-    return redirect('/Messages/'+e)
-    
+    return redirect('/Messages/' + e)
+
+
 class NachrichtenView(ListView):
     template_name = 'nachrichten.html'
-    
+
     def get_queryset(self):
         usr = str(self.request.user)
-        q = Nachrichten.objects.filter((Q(empfaenger = usr) & Q(absender = str(self.kwargs['author'])))
-                                        | (Q(absender = usr) & Q(empfaenger = str(self.kwargs['author']))))
+        q = Nachrichten.objects.filter((Q(empfaenger=usr) & Q(absender=str(self.kwargs['author'])))
+                                       | (Q(absender=usr) & Q(empfaenger=str(self.kwargs['author']))))
         return q
 
     def get_context_data(self, *args, **kwargs):
         context = super(NachrichtenView, self).get_context_data(*args, **kwargs)
         usr = str(self.request.user)
-        context['conversations'] = Nachrichten.objects.filter(Q(absender = usr)
-                                                              | Q(empfaenger = usr)).exclude(empfaenger = usr).values('empfaenger').distinct('empfaenger')
+        context['conversations'] = Nachrichten.objects.filter(Q(absender=usr)
+                                                              | Q(empfaenger=usr)).exclude(empfaenger=usr).values(
+            'empfaenger').distinct('empfaenger')
         return context 
     
