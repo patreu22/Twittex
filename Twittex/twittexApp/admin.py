2188e7d1041572570afd0883719e94ec3e15ee2e
patreu22 <patrickreuter@outlook.com>
2015-06-10 19:38:43 +0200

    Many more or less fancy frontend updates

--- Twittex/twittexApp/admin.py
+++ Twittex/twittexApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import Posts, UserProfile
+from .models import Posts, UserProfile, Nachrichten
 
 admin.site.register(Posts)
 admin.site.register(UserProfile)
+admin.site.register(Nachrichten)
